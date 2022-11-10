60d78047ba4d45309bf59ace20c6839d36cc87c2
Igor Galo <galoigor8@gmail.com>
2022-11-10 20:48:29 -0300

    atualizacao da aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
@@ -6,8 +7,11 @@ import { StyledTimeline } from "../src/components/Timeline";
 
 function HomePage() {
     // const estilosDaHomePage = { backgroundColor: 'red' };
+    // console.log(config.playlists)
 
-    console.log(config.playlists)
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("");
+
+
 
     return (
         <>
@@ -18,9 +22,9 @@ function HomePage() {
                 flex: 1,
 
             }}>
-                <Menu />
+                <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
                 <Header />
-                <Timeline playlists={config.playlists} >
+                <Timeline searchValue = {valorDoFiltro} playlists={config.playlists} >
                     Conteúdo
                 </Timeline>
             </div>
@@ -45,7 +49,6 @@ const StyledHeader = styled.div`
         border-radius:50%;
     }
     .user-info{
-        margin-top: 50px;
         display: flex;
         align-items: center;
         width: 100%;
@@ -54,10 +57,15 @@ const StyledHeader = styled.div`
         background-color: #e5e5e5;
     }
 `;
+const Styledbanner = styled.div`
+    background-color: blue;
+    background-image: url("https://images.unsplash.com/photo-1667958650125-5c452fc9c530?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80");
+    height: 230px;
+`;
 function Header() {
     return (
         <StyledHeader>
-            {/* <img src="banner" /> */}
+            <Styledbanner />
 
             <section className="user-info">
                 <img src={`https://github.com/${config.github}.png`} />
@@ -74,8 +82,8 @@ function Header() {
     )
 }
 
-function Timeline(props) {
-    console.log("Dentro do componente", props)
+function Timeline({searchValue, ...props}) {
+    // console.log("Dentro do componente", props)
     const playlistNames = Object.keys(props.playlists);
 
     return (
@@ -83,15 +91,19 @@ function Timeline(props) {
             Timeline
             {playlistNames.map((playlistName) => {
                 const videos = props.playlists[playlistName];
-                console.log(playlistName)
-                console.log(videos)
+                // console.log(playlistName)
+                // console.log(videos)
                 return (
-                    <section>
+                    <section key={playlistName}>
                         <h2>{playlistName}</h2>
                         <div>
-                            {videos.map((video) => {
+                            {videos.filter((video)=>{
+                                const titleNormalized = video.title.toLowerCase();
+                                const searchValueNormalized = searchValue.toLowerCase();
+                                return titleNormalized.includes(searchValueNormalized)
+                            }).map((video) => {
                                 return (
-                                    <a href={video.url}>
+                                    <a key={video.url} href={video.url}>
                                         <img src={video.thumb} />
                                         <span>
                                             {video.title}
--- src/components/Menu/Index.js
+++ src/components/Menu/Index.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
+import Search from "./components/Search";
 
 const StyledMenu = styled.header`
   display: flex;
   flex-direction: row;
   height: 56px;
   justify-content: space-between;
-  background-color: ${({ theme }) => theme.backgroundLevel1 || "#e5e5e5"};
+  background-color: ${({ theme }) => theme.backgroundLevel1 || "#FFFFFF"};
   border: 1px solid ${({ theme }) => theme.borderBase || "#e5e5e5"};
   align-items: center;
   padding: 0 16px;
@@ -24,15 +25,16 @@ const StyledMenu = styled.header`
   }
 `;
 
-export default function Menu() {
+export default function Menu({valorDoFiltro, setValorDoFiltro}) {
   return (
     <StyledMenu>
       <div>
         <Logo />
       </div>
+      <Search valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro}/>
     </StyledMenu>
   );
-}
+} 
 
 function Logo() {
   return (
